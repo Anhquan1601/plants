b23a1488f45299ab14af84a8218a9b5db23be029
Anhquan1601 <157450094+Anhquan1601@users.noreply.github.com>
2024-04-18 20:51:49 +0700

    Add files via upload

--- responsive-plants-website-main/responsive-plants-website-main/assets/js/main.js
+++ responsive-plants-website-main/responsive-plants-website-main/assets/js/main.js
@@ -100,7 +100,31 @@ function SendMail() {
     
 }
 
+/*=============== Card ===============*/
+const btn = document.querySelectorAll("article button")
+//console.log(btn)
+btn.forEach(function(button,index){
+    button.addEventListener("click",function(event){{
+        var btnItem = event.target 
+        var product = btnItem.parentElement
+        var productImg = product.querySelector("img").src
+        var productName = product.querySelector("h3").innerText
+        var productPrice = product.querySelector("span").innerText
+        //console.log(productPrice,productImg,productName)
+        addcart(productPrice,productImg,productName)
 
+    }})
+
+})
+function addcart(productPrice,productImg,productName) {
+    var addtr = document.createElement("tr")
+    var trcontent = '<tr><td style="display: flex;align-items: center;"><img style="width: 70px" src="' +productImg+ '" alt="">' +productName+ '</td><td><p><span>' +productPrice+ '</span><sup>d</sup></p></td><td><input style="width: 30px; outline: none;" type="number" value="1" min="1"></td><td style="cursor: pointer;">Xoa</td></tr>'
+    addtr.innerHTML = trcontent
+    var cartTable = document.querySelector("tbody")
+     console.log(cartTable)
+     cartTable.append(addtr)
+}
+//------total price--------//
 
 
 
